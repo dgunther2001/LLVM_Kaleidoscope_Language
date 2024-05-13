934e15310e71cd031032b0b6ff451680bfcf15da
dgunther2001 <danielgunther2001@gmail.com>
2024-05-13 13:34:52 -0400

    Adjusted main driver loop to output generated llvm ir

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,49 @@
 #include <iostream>
-#include "../include/kaleidoscope/ast.h"
+#include "../include/kaleidoscope/AST.h"
 #include "../include/kaleidoscope/parser.h"
 #include "../include/kaleidoscope/lexer.h"
+#include "../include/kaleidoscope/codegen.h"
 
+static void InitializeModule() {
+    TheContext = std::make_unique<llvm::LLVMContext>(); // initializes an llvm context object
+    TheModule = std::make_unique<llvm::Module>("Just in Time (JIT) Compiler", *TheContext); // initializes an llvm module to hold functions and other global declarations
+
+    Builder = std::make_unique<llvm::IRBuilder<>>(*TheContext); // declares the ir builder, which is passed a pointer to the context object
+}
 
 static void HandleDefinition() {
-    if (ParseDefinition()) { // parse a function definition 
-        fprintf(stderr, "Parsed a function definition.\n"); // say that it was parsed correctly as an error message
-    } else {
-        getNextToken(); // otherwise go to the next token
+    if (auto FnAST = ParseDefinition()) { // parse the function definition
+        if (auto* FnIR = FnAST->codegen()) { // generate llvm ir from the definition
+            fprintf(stderr, "Read function definition: "); // print out the generated ir (next 2 lines as well)
+            FnIR->print(llvm::errs());
+            fprintf(stderr, "\n");
+        } 
+    } else { // error handling
+        getNextToken();
     }
 }
 
 static void HandleDecl() {
-    if (ParseDecl()) { // parse a declaration
-        fprintf(stderr, "Parsed a function declaration.\n"); // indicate that we have parsed a declaration as an error message
+    if (auto ProtoAST = ParseDecl()) { // parse the function delcaration into an AST node
+        if (auto* FnIR = ProtoAST->codegen()) { // generate llvm ir for the function delcaration
+            fprintf(stderr, "Read function declaration: "); // print out the ir
+            FnIR->print(llvm::errs());
+            fprintf(stderr, "\n");
+        }
     } else {
         getNextToken();
     }
 }
 
 static void HandleTopLevelExpression() {
-    if (ParseTopLevelExpr()) {
-        fprintf(stderr, "Parsed a top level expression.\n"); // indicate that we have parsed a top-level expression
+    if (auto FnAST = ParseTopLevelExpr()) {
+        if (auto* FnIR = FnAST->codegen()) {
+            fprintf(stderr, "Read top-level expression: "); // indicate that we have parsed a top-level expression 
+            FnIR->print(llvm::errs());
+            fprintf(stderr, "\n");
+
+            FnIR->eraseFromParent(); // removes the delcaration from the module, cleaning up after it has been printed 
+        }
     } else {
         getNextToken();
     }
@@ -62,7 +83,11 @@ int main() {
     fprintf(stderr, ">> "); // prime the inital token
     getNextToken(); // go the the next one...
 
+    InitializeModule();
+
     MainLoop(); // run the maininterpreter loop
+
+    TheModule->print(llvm::errs(), nullptr);
 
     return 0;
 }
