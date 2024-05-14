be293d4aeea937536391f5422ed090618132f90e
dgunther2001 <danielgunther2001@gmail.com>
2024-05-14 11:39:54 -0400

    Added an expression handler that acts as the pass manager and mdoule manager

---
+++ include/kaleidoscope/expression_handler.h
@@ -0,0 +1,55 @@
+#ifndef EXPRESSION_HANDLER_H
+#define EXPRESSION_HANDLER_H
+
+#include "../../external_libs/KaleidoscopeJIT.h"
+
+#include "lexer.h"
+#include "parser.h"
+#include "codegen.h"
+
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/StandardInstrumentations.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Transforms/InstCombine/InstCombine.h"
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/Transforms/Scalar/GVN.h"
+#include "llvm/Transforms/Scalar/Reassociate.h"
+#include "llvm/Transforms/Scalar/SimplifyCFG.h"
+
+extern std::unique_ptr<llvm::orc::KaleidoscopeJIT> TheJIT;
+extern llvm::ExitOnError ExitOnErr;
+
+extern std::unique_ptr<llvm::FunctionPassManager> TheFPM;
+extern std::unique_ptr<llvm::LoopAnalysisManager> TheLAM;
+extern std::unique_ptr<llvm::FunctionAnalysisManager> TheFAM;
+extern std::unique_ptr<llvm::CGSCCAnalysisManager> TheCGAM;
+extern std::unique_ptr<llvm::ModuleAnalysisManager> TheMAM;
+extern std::unique_ptr<llvm::PassInstrumentationCallbacks> ThePIC;
+extern std::unique_ptr<llvm::StandardInstrumentations> TheSI;
+
+extern void InitializeModuleAndManagers(void);
+extern void HandleDefinition();
+extern void HandleDecl();
+extern void HandleTopLevelExpression();
+extern void MainLoop();
+
+
+
+
+
+
+
+#endif
